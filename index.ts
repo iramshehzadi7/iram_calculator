12ee3ccbd1530f81d0566020c7aaa81b0123bbcd
iramshehzadi7 <150755376+iramshehzadi7@users.noreply.github.com>
2024-02-29 20:08:58 +0500

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,44 @@
+import inquirer from "inquirer"
+ 
+const answers :{
+    numberOne: number,
+    numberTwo:number,
+    operator:string,
+} = await inquirer.prompt([
+{
+        type:"number",
+        name:"numberOne",
+        message:"kindly enter your first no:"
+},
+{
+        type:"number",
+        name:"numberTwo",
+        message:"kindly enter your second no:"
+},
+{
+        type:"list",
+        name:"operator",
+        choices:["*","+","-","/"],
+        message:"select operator:"
+} ,   
+]);
+const {numberOne, numberTwo, operator} = answers;
+if(numberOne && numberTwo && operator){
+    let result: number = 0;
+    if(operator === "+"){
+        result = numberOne +numberTwo
+    }else if(operator === "-"){
+        result = numberOne - numberTwo
+    } if(operator === "/"){
+        result = numberOne / numberTwo
+    } if(operator ==="*"){
+        result = numberOne * numberTwo
+    }
+
+    console.log("your result is:",result)
+}else{
+    console.log("invalid result ");
+    
+}
+
+ 
